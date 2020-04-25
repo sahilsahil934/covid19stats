ff060e92051a47a67c9f634962a958099b5bd42d
iamsahil1910 <iamsahil1910@gmail.com>
2020-04-26 00:40:31 +0530

    removed extra data

--- src/api/totalCases.js
+++ src/api/totalCases.js
@@ -1,36 +1,7 @@
 import axios from 'axios'
 
-// const totalCases = () => {
-
-//     axios.get("https://api.covid19india.org/data.json")
-//   .then(response => {
-//     console.log(response.data);
-//     let data = response.data;
-//         return data;
-//   });
-// }
-
 export default axios.create({
     baseURL: "https://api.thevirustracker.com/free-api?global=stats" 
 });
 
-// class TotalCases extends React.Component {
 
-//     state = {
-//         total: 0,
-//         death: 0,
-//         recovered: 0
-//     };
-
-//     componentDidMount() {
-
-//         axios.get(`https://jsonplaceholder.typicode.com/users`)
-//         .then(res => {
-//         const persons = res.data;
-//         this.setState({ persons });
-//       })
-//     }
-
-// }
-
-// export default TotalCases; 
