c8c8a7f0525cc96c11cf47cd191c045c1e8c0a84
iamsahil1910 <iamsahil1910@gmail.com>
2020-04-26 11:16:06 +0530

    added

--- src/components/title.js
+++ src/components/title.js
@@ -7,7 +7,7 @@ class Title extends React.Component {
         return (
             <div className="ui one center aligned fixed menu">
                     <a href="/"><h2 className="ui center aligned item">Covid-19 Stats</h2></a>
-                    <h1 style={{margin: 'auto'}} className="ui center  ">Stay Home, Stay Safe</h1>
+                    <h3 style={{margin: 'auto'}} className="ui center  ">Stay Home, Stay Safe</h3>
                 
             </div>
         );
